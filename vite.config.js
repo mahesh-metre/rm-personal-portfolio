08fb12ce3861427e24a5fc2f9f019efc5049a7f6
Mahesh <maheshmetre2005@gmail.com>
2025-07-14 23:31:46 +0530

    added more projects and few changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
+import { readdirSync } from 'fs';
+
+const htmlFiles = readdirSync(__dirname)
+  .filter(file => file.endsWith('.html'))
+  .reduce((entries, file) => {
+    const name = file.replace('.html', '');
+    entries[name] = resolve(__dirname, file);
+    return entries;
+  }, {});
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: htmlFiles
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
